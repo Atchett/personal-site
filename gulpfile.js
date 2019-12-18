b0b8057c3fda6841a87b526ca31a40df11663be8
Atchett <Atchett@users.noreply.github.com>
2019-12-18 12:33:59 +0000

    removed font-awesome local for cdn version

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,9 @@
 // Importing specific gulp API functions lets us write them below as series() instead of gulp.series()
 const { src, dest, series, parallel } = require("gulp");
 // Importing all the Gulp-related packages we want to use
-//const sass = require("gulp-sass");
 const prefix = require("gulp-autoprefixer");
 const stripCssComments = require("gulp-strip-css-comments");
 const csso = require("gulp-csso");
-//const rename = require("gulp-rename");
 const uglify = require("gulp-uglify");
 const del = require("del");
 const cache = require("gulp-cache");
@@ -25,54 +23,15 @@ const srcFiles = {
   cssPath: dir.src + "/assets/css/**/*.css",
   jsPath: dir.src + "/assets/js/**/*.js",
   htmlPath: dir.src + "/**/*.html",
-  images: dir.src + "/images/**/*.*",
-  fonts: dir.src + "/assets/webfonts/*"
+  images: dir.src + "/images/**/*.*"
 };
 
 const distFiles = {
   css: dir.dist + "/assets/css",
   js: dir.dist + "/assets/js",
   html: dir.dist,
-  images: dir.dist + "/images",
-  fonts: dir.dist + "/assets/webfonts"
+  images: dir.dist + "/images"
 };
-
-// // SASS task
-// function scssTask() {
-//   return (
-//     src(srcFiles.scssPath)
-//       //compile sass
-//       .pipe(
-//         sass({
-//           outputStyle: "nested",
-//           precision: 10,
-//           includePaths: ["."],
-//           onError: console.error.bind(console, "Sass error:")
-//         })
-//       )
-//       // autoprefix
-//       .pipe(prefix("last 2 versions"))
-//       //push .css to output
-//       //.pipe(dest(distFiles.css))
-//       // remove comments for minify
-//       .pipe(
-//         stripCssComments({
-//           preserve: true
-//         })
-//       )
-//       // minify
-//       .pipe(
-//         csso({
-//           restructure: false
-//         })
-//       )
-//       // rename .min
-//       .pipe(rename({ suffix: ".min" }))
-//       // output .min file
-//       .pipe(dest(distFiles.css))
-//   );
-// }
-// exports.scssTask = scssTask;
 
 function cssTask() {
   return (
@@ -85,27 +44,18 @@ function cssTask() {
           preserve: true
         })
       )
-      //push to src styles folder
-      //.pipe(dest(distFiles.css))
-      //push unminified CSS to the output path
-      //.pipe(dest(distFiles.css))
       // Minify the file
       .pipe(
         csso({
           restructure: false
         })
       )
+      // remove any unnecessary css
       .pipe(
         purgecss({
           content: [srcFiles.htmlPath]
         })
       )
-      // change the filename to add min
-      //.pipe(rename({ suffix: ".min" }))
-      //push to src styles folder
-      // .pipe(
-      //   dest(dir.src + '/style')
-      // )
       // Output
       .pipe(dest(distFiles.css))
   );
@@ -117,19 +67,11 @@ function scriptsTask() {
     src(srcFiles.jsPath)
       // Minify the file
       .pipe(uglify())
-      // change the filename to add min
-      //.pipe(rename({ suffix: ".min" }))
       // Output
       .pipe(dest(distFiles.js))
   );
 }
 exports.scriptsTask = scriptsTask;
-
-// essentially copy the fonts across
-function fontsTask() {
-  return src(srcFiles.fonts).pipe(dest(distFiles.fonts));
-}
-exports.fontsTask = fontsTask;
 
 function imageTask() {
   return src(srcFiles.images)
@@ -171,6 +113,6 @@ exports.clearCacheTask = clearCacheTask;
 exports.default = series(
   cleanTask,
   clearCacheTask,
-  parallel(cssTask, scriptsTask, imageTask, fontsTask),
+  parallel(cssTask, scriptsTask, imageTask),
   htmlTask
 );
